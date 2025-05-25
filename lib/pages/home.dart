073bc15e02dce987e8caf58e22ab72cae4669936
dudamatos <dudasantosmatos15@gmail.com>
2025-05-25 15:39:12 -0300

    add style in home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,17 +1,21 @@
-import 'package:flutter/cupertino.dart';
+import 'package:ap2/styles/home_style.dart';
 import 'package:flutter/material.dart';
-
 import '../models/countries.dart';
 import '../service/countries_service.dart';
 import 'details_countries.dart';
+
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Paises do Mundo',
+      title: 'Países do Mundo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        scaffoldBackgroundColor: HomeStyles.secondaryColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: HomeStyles.primaryColor,
+        ),
       ),
       home: const MyHomePage(title: 'Países do Mundo'),
     );
@@ -40,46 +44,89 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: HomeStyles.secondaryColor,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text(
+          'Países do Mundo',
+          style: HomeStyles.appBar,
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 12),
+            child: SizedBox(
+              width: 50,
+              height: 50,
+              child: Image.asset(
+                'assets/rosa_dos_ventos.png',
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
       ),
       body: FutureBuilder<List<Countries>>(
         future: _countriesFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
+            return const Center(
+              child: CircularProgressIndicator(color: Colors.white),
+            );
           }
 
           if (snapshot.hasError) {
-            return Center(child: Text("Erro ao carregar os países.${snapshot.error}"));
+            return Center(
+              child: Text(
+                "Erro ao carregar os países: ${snapshot.error}",
+                style: const TextStyle(color: Colors.white),
+              ),
+            );
           }
 
           if (snapshot.hasData) {
             final countries = snapshot.data ?? [];
 
             return ListView.separated(
+              padding: const EdgeInsets.all(15),
               itemCount: countries.length,
-              separatorBuilder: (_, __) => const Divider(),
+              separatorBuilder: (_, __) => const SizedBox(height: 25),
               itemBuilder: (context, index) {
                 final country = countries[index];
-                return ListTile(
-                  title: Text(country.name),
-                  trailing: IconButton(onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => DetailsCountry(country: country)),
-                    );
-                  } , icon: Icon(Icons.info_outline)),
+                return Container(
+                  decoration: HomeStyles.cardBoxDecoration,
+                  padding: const EdgeInsets.all(10),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    title: Text(
+                      country.name,
+                      style: HomeStyles.countryName,
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.info_outline, color: Colors.white),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailsCountry(country: country),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 );
               },
             );
           }
 
-          return const Center(child: Text("Nenhum dado encontrado."));
+          return const Center(
+            child: Text(
+              "Nenhum dado encontrado.",
+              style: TextStyle(color: Colors.white),
+            ),
+          );
         },
       ),
     );
   }
 }
-
---
+++ lib/styles/home_style.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeStyles {
+  static const Color primaryColor = Color(0xFF3C506E);
+  static const Color secondaryColor = Color(0xFF88A3CC);
+  static const Color boxColor = Color(0xFF5F8CAF);
+  static const Color infoColor = Color(0xFFFFFFFF);
+
+  static const TextStyle appBar = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+  );
+
+  static const TextStyle countryName = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static BoxDecoration cardBoxDecoration = BoxDecoration(
+    color: boxColor,
+    borderRadius: BorderRadius.circular(12)
+  );
+
+}
